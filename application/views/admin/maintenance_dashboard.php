e543e733b9d54a2de09b6cab13824aecf3827876
Mushfequr Rahman <me.vgithub@gmail.com>
2025-07-12 09:44:20 +0600

    after laptop change and before ie,maintenance dashboard

--- application/views/admin/maintenance_dashboard.php
+++ application/views/admin/maintenance_dashboard.php
@@ -113,7 +113,8 @@
 												</div>
 												<div class="col-md-6">
 													<div class="float-right" style="text-align: right;">
-														<button id="downloadExcel" class="btn btn-success btn-xs">Download As Excel</button>
+														<button id="downloadExcel" class="btn btn-success btn-xs"><i class="fa fa-file-excel-o" aria-hidden="true"></i>
+														</button>
 													</div>
 												</div>
 											</div>
@@ -124,7 +125,7 @@
 													<table id="tableData" class="table table-hover table-bordered">
 														<thead style="background:#91b9e6;">
 															<tr>
-																<th data-column="0"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="1" checked><strong>SL</strong></label></th>
+															<th data-column="0"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="1" checked><strong>SL</strong></label></th>
 																<th data-column="1"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="2" checked><strong>Factory</strong></label></th>
 																<th data-column="2"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="3" checked><strong>Name</strong></label></th>
 																<th data-column="3"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="4" checked><strong>Purpose</strong></label></th>
@@ -133,13 +134,15 @@
 																<th data-column="6"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="7" checked><strong>Repair</strong></label></th>
 																<th data-column="7"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="8" checked><strong>Transfer</strong></label></th>
 																<th data-column="8"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="9" checked><strong>Rent</strong></label></th>
-																<th data-column="9"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="10" checked><strong>Total Free Machine</strong></label></th>
-																<th data-column="10"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="11" checked><strong>Total Purchase Machine</strong></label></th>
+																<th data-column="9"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="10" checked><strong>In Transit Sent</strong></label></th>
+																<th data-column="10"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="11" checked><strong>In Transit Receiveable</strong></label></th>
+																<th data-column="11"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="12" checked><strong>Total Free Machine</strong></label></th>
+																<th data-column="12"><label class="checkbox-inline"><input type="checkbox" class="column-select" data-col-index="13" checked><strong>Total IN House Machine</strong></label></th>
 															</tr>
 														</thead>
 														<tfoot>
 															<tr>
-																<th id="rowCount"></th>
+															<th id="rowCount"></th>
 																<th>&nbsp;</th>
 																<th>&nbsp;</th>
 																<th>&nbsp;</th>
@@ -150,6 +153,9 @@
 																<th data-math="col-sum">col-sum</th>
 																<th data-math="col-sum">col-sum</th>
 																<th data-math="col-sum">col-sum</th>
+																<th data-math="col-sum">col-sum</th>
+																<th data-math="col-sum">col-sum</th>
+																
 															</tr>
 														</tfoot>
 														<tbody>
@@ -158,14 +164,47 @@
 															foreach ($ul as $row) { ?>
 																<tr>
 																	<td style="vertical-align:middle;"><label class="checkbox-inline"><input type="checkbox" class="row-select" checked><?php echo $i++; ?></label></td>
-																	<td style="vertical-align:middle;"><?php echo $row['pfactoryid']; ?></td>
+																	<td style="vertical-align:middle;"><?php echo $row['factoryid']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['mname']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['mpurpose']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['mtype']; ?></td>
-																	<td style="vertical-align:middle;"><?php echo $row['totalmachineinline']+$row['totalmachineinliner']; ?></td>
-																	<td style="vertical-align:middle;"><?php echo $row['totalmachineinrepair']+$row['totalmachineinrepairr']; ?></td>
+																	<td style="vertical-align:middle;"><?php echo $row['totalmachineinline']; ?></td>
+																	<td style="vertical-align:middle;"><?php echo $row['totalmachineinrepair']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['totalmachinetransfer']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['totalmachinerent']; ?></td>
+																	<td style="vertical-align:middle;"><?php echo $row['totalmachineintransitsent']; ?></td>
+																	<td style="vertical-align:middle;"><?php echo $row['totalmachineintransitreceiveable']; ?></td>
+																	
+																	
+																	<!-- <?php
+																	if(($row['factoryid']==$row['pfactoryid'])&& ($row['factoryid']!=$row['cfactoryid']))
+																	{
+																		?>
+																		<td style="vertical-align:middle;"><?php echo $row['totalmachinetransfer']; ?></td>
+																		<?php
+																	}
+																	else
+																	{
+																		?>
+																		<td style="vertical-align:middle;">0</td>
+																		
+																		<?php
+																	}
+																	if(($row['factoryid']!=$row['pfactoryid'])){
+																		?>
+																		<td style="vertical-align:middle;"><?php echo $row['totalmachinetransfer']; ?></td>
+																		<?php
+																	}
+																	else
+																	{
+																		?>
+																		<td style="vertical-align:middle;">0</td>
+																		
+																		<?php
+																	}
+																	?> -->
+
+																	
 																	<td style="vertical-align:middle;"><?php echo $row['totalfreemachine']+$row['totalfreemachiner']; ?></td>
 																	<td style="vertical-align:middle;"><?php echo $row['totalinhousemachine']; ?></td>
 																</tr>
